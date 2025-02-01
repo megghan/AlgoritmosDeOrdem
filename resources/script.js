df7c996fbef46c6da8239459047a0080d3263d80
megghan <93269741+megghan@users.noreply.github.com>
2025-01-31 22:16:46 -0300

    Bubble sort

--- resources/script.js
+++ resources/script.js
@@ -16,6 +16,8 @@ async function generateListNumbers(){
     }
     console.log(listNumbers);
     createNumberElement(listNumbers);
+    
+
 }
 
 async function createNumberElement(listNumbers) {
@@ -28,8 +30,51 @@ async function createNumberElement(listNumbers) {
     });
 }
 
-// async function bubbleSort(listNumbers) {
-//     if(listNumbers!=null){
-//         //
-//     }
-// }
+async function bubbleSort(listNumbers) {
+    // implementar func de botao ordenar desabilitado até usuario
+    // clicar em gerar os dados
+
+
+    let totalNumbers = listNumbers.length;
+    let elementos = document.querySelectorAll(".numero");
+
+    for(let i=0; i<totalNumbers; i++){
+        for(let j=0; j<totalNumbers-i-1; j++){
+
+            elementos[j].style.backgroundColor = "red";
+            elementos[j + 1].style.backgroundColor = "red";
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            if( listNumbers[j] > listNumbers[j+1]){
+                let aux = listNumbers[j];
+                listNumbers[j] = listNumbers[j+1];
+                listNumbers[j+1]= aux;
+
+                await trocaElementos(elementos[j], elementos[j + 1]);
+                elementos = document.querySelectorAll(".numero");
+
+            }
+            elementos[j].style.backgroundColor = "";
+            elementos[j + 1].style.backgroundColor = "";
+        }
+    }
+    console.log(listNumbers);
+    
+}
+async function trocaElementos(elemento1, elemento2) {
+    return new Promise(resolve => {
+        let parent = elemento1.parentNode;
+        // Criando um elemento temporário
+        let temp = document.createElement("div");
+        parent.insertBefore(temp, elemento1);
+
+        // Troca visual
+        parent.insertBefore(elemento1, elemento2);
+        parent.insertBefore(elemento2, temp);
+
+        // Remove o temporário
+        parent.removeChild(temp);
+
+        setTimeout(resolve, 500);
+    });
+}
